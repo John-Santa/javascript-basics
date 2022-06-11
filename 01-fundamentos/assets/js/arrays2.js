7b4bb33f56e459cf33148cd98c14267ea0e132cc
John Santa <andersonsanta3@gmail.com>
2022-06-11 13:08:02 -0500

    More information about arrays

---
+++ 01-fundamentos/assets/js/arrays2.js
@@ -0,0 +1,10 @@
+/**
+ *  *Arrays Methods
+ */
+
+let games = ['Super Mario', 'GTA V', 'Crash Bandicol', 'Fifa'];
+console.log(`Size of the array: ${games.length}`);
+
+games.forEach( (element, index, arr) => {
+    console.log(`${index}: ${element}, in the array: ${arr}`);
+});
