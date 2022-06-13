2fc60ca7b61694fb95dbe022aeb1b73c53332581
John Santa <andersonsanta3@gmail.com>
2022-06-13 16:24:15 -0500

    Create deck

---
+++ 02-blackjack/assets/js/game.js
@@ -0,0 +1,20 @@
+/**
+ * 2C Tow of Clubs.
+ * 2D Two of Diamonds.
+ * 2H Two of Hearts.
+ * 2S Two of Spades.
+ */
+
+let deck = [];
+
+const createDeck = () => {
+    const suits = ['C', 'D', 'H', 'S'];
+    const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    for (let suit of suits) {
+        for (let value of values) {
+            deck.push(value + suit);
+        }
+    }
+}
+
+createDeck();
