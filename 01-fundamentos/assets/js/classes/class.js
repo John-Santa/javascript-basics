e0d71c9aa50afaffefe4d86b259eb80dd1800a90
John Santa <andersonsanta3@gmail.com>
2022-06-21 18:20:06 -0500

    static methods

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -3,11 +3,28 @@ class NaturalPerson {
     name;
     lastName;
     age;
+    food;
+
+    //* Static methods
+    static _count = 0;
 
     constructor(name, lastName, age) {
         this.name = name;
         this.lastName = lastName;
         this.age = age;
+        NaturalPerson._count ++;
+    }
+
+    set setFood(food) {
+        this.food = food.toUpperCase();
+    }
+
+    get getFood() {
+        return this.food;
+    }
+
+    static get count() {
+        return NaturalPerson._count
     }
 
     whoIAm() {
@@ -17,6 +34,14 @@ class NaturalPerson {
 }
 
 const john = new NaturalPerson('John', 'Santa', 26);
+const maicol = new NaturalPerson('Maicol', 'Santa',19);
 console.log(john);
 
 console.log(john.whoIAm());
+john.setFood = 'Lasagna';
+console.log({
+    john
+});
+
+console.log(john.getFood);
+console.log(NaturalPerson.count);
