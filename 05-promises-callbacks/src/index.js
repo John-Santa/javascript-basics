8099bc987a3de72f21f53569a65e76385fb19a5b
John Santa <andersonsanta3@gmail.com>
2022-07-02 23:32:06 -0500

    Async await

--- 05-promises-callbacks/src/index.js
+++ 05-promises-callbacks/src/index.js
@@ -1,5 +1,4 @@
 import './styles.css';
-import { findHero, findHeroAsync } from './js/promises';
+import { obtainHeroArr } from './js/await.js';
 
-findHero('capi').then( console.log );
-findHeroAsync('iron').then( console.log );
+obtainHeroArr().then( console.table );
